b4a0d20c4fabff8cd56250e293554a7b178454b4
Ahmed Monis <monis@192.168.1.120>
2026-02-19 15:19:34 +0500

    ux

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Redirect to profile selection if no profile chosen
 document.addEventListener("DOMContentLoaded", function() {
-    if (window.location.pathname.includes("index.html")) {
+    if (window.location.pathname.includes("home.html")) {
         const profile = localStorage.getItem("netflyProfile");
         if (!profile) {
-            window.location.href = "profiles.html";
+            window.location.href = "index.html";
         } else {
             document.getElementById("profile-name").innerText = profile;
         }
@@ -13,11 +13,43 @@ document.addEventListener("DOMContentLoaded", function() {
 // Profile selection
 function selectProfile(name){
     localStorage.setItem("netflyProfile", name);
-    window.location.href = "index.html";
+    window.location.href = "home.html";
 }
 
 // Logout / switch profile
 function switchProfile(){
     localStorage.removeItem("netflyProfile");
-    window.location.href = "profiles.html";
+    window.location.href = "index.html";
 }
+function toggleMute(){
+    const video = document.getElementById("heroVideo");
+    const soundOn = document.getElementById("iconSoundOn");
+    const muted = document.getElementById("iconMuted");
+
+    video.muted = !video.muted;
+
+    if(video.muted){
+        soundOn.style.display = "none";
+        muted.style.display = "block";
+    } else {
+        soundOn.style.display = "block";
+        muted.style.display = "none";
+    }
+}
+
+window.addEventListener("scroll", function(){
+    const video = document.getElementById("heroVideo");
+    const heroSection = document.querySelector(".hero");
+
+    if(!video || !heroSection) return;
+
+    const rect = heroSection.getBoundingClientRect();
+
+    // If hero is out of view
+    if(rect.bottom <= 0){
+        video.pause();
+    } else {
+        video.play();
+    }
+});
+
